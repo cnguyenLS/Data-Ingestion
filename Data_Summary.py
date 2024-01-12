908dc16c377391859363d33cb191aadc4f96b5d5
cnguyenLS <148390808+cnguyenLS@users.noreply.github.com>
2024-01-12 17:17:40 -0500

    Update Data_Summary.py

--- Data_Summary.py
+++ Data_Summary.py
@@ -396,8 +396,8 @@ if uploaded_trans is not None:
                         st.markdown("<font color='green'>There are no missing dates. No data to show here. </font>",unsafe_allow_html=True)
                 
                 with col2: 
-                    #missing_loc['Total_Sales'] = missing_loc['Total_Sales'].apply(format_number)
-                    if len(missing_loc) > 0:
+                    missing_loc['Total_Sales'] = missing_loc['Total_Sales'].apply(format_number)
+                    if len(missing_loc) > 0 :
                         st.markdown(
                             f"<font color='red'> Warning: <br> The following information pertains to unmatched storeIDs.</font>",
                             unsafe_allow_html=True
